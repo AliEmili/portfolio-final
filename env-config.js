b9bee97af4278bc35b211f567455d3b54bca708e
aliemili <aliemili2013@yahoo.com>
2021-10-05 18:18:28 +0330

    fix audience

--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BASE_URL': prod ? 'https://emili-portfolio.herokuapp.com/' : 'http://localhost:3000',
-  'process.env.NAMESPACE': 'https://emili-portfolio.herokuapp.com/',
-  'process.env.CLIENT_ID': 'NfvS9nw81ItncHJKPHCaAvwD9ChNWYn3'
+  'process.env.BASE_URL': prod ? 'https://emili-portfolio.herokuapp.com' : 'http://localhost:3000',
+  'process.env.NAMESPACE': 'https://emili-portfolio.herokuapp.com',
+  'process.env.CLIENT_ID': '37pk6xdrh9ZXgqD6yhy448fOrSH32szD'
 }
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
   DB_URI: process.env.DB_URI,
-  NAMESPACE: 'https://emili-portfolio.herokuapp.com/'
+  NAMESPACE: 'https://emili-portfolio.herokuapp.com'
 }
 
